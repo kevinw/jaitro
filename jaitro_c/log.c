056241b0d581f04558f5cfa0270fe264b8edcabf
Kevin Watters <kevinwatters@gmail.com>
2025-03-13 11:47:51 +0100

    core_log working; README

--- jaitro_c/log.c
+++ jaitro_c/log.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+// workaround for jai not supporting varargs in #c_call yet
 void core_log(int32_t level, const char* fmt, ...) {
     va_list args;
     va_start(args, fmt);
+    printf("[%d] ", level);
     vprintf(fmt, args);
     va_end(args);
 }
